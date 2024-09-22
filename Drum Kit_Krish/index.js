304780d02bfdb102c7a53f8e6bb6466a90d80031
nskbobby <80738068+nskbobby@users.noreply.github.com>
2024-09-22 17:51:45 -0500

    Update index.js(Switch case)

--- Drum Kit_Krish/index.js
+++ Drum Kit_Krish/index.js
@@ -5,32 +5,67 @@ for(var i=0;i<alldrumbuttonslength;i++){
   document.querySelectorAll(".drum")[i].addEventListener("click",playsound);
   }
   function playsound(){
+var clickedButton=this.innerHTML;
 
-    if(this.innerHTML=='w'){
-       var aud=new Audio('sounds/crash.mp3');
-     aud.play();
-  }else if(this.innerHTML=='a'){
-     var aud=new Audio('sounds/kick-bass.mp3');
-   aud.play();
-}
-else if(this.innerHTML=='s'){
-   var aud=new Audio('sounds/snare.mp3');
+switch(clickedButton){
+case 'w':
+   var aud=new Audio('sounds/crash.mp3');
  aud.play();
+ break;
+case 'a':
+ var aud=new Audio('sounds/kick-bass.mp3');
+aud.play();
+break;
+case 's':
+var aud=new Audio('sounds/snare.mp3');
+aud.play();
+break;
+case 'd':
+var aud=new Audio('sounds/tom-1.mp3');
+aud.play();
+break;
+case 'j':
+var aud=new Audio('sounds/tom-2.mp3');
+aud.play();
+break;
+case 'k':
+var aud=new Audio('sounds/tom-3.mp3');
+aud.play();
+break;
+case 'l':
+var aud=new Audio('sounds/tom-4.mp3');
+aud.play();
 }
-else if(this.innerHTML=='d'){
-   var aud=new Audio('sounds/tom-1.mp3');
- aud.play();
-}
-else if(this.innerHTML=='j'){
-   var aud=new Audio('sounds/tom-2.mp3');
- aud.play();
-}
-else if(this.innerHTML=='k'){
-   var aud=new Audio('sounds/tom-3.mp3');
- aud.play();
-}
-else if(this.innerHTML=='l'){
-   var aud=new Audio('sounds/tom-4.mp3');
- aud.play();
-}
+
+
+
+
+
+//     if(this.innerHTML=='w'){
+//        var aud=new Audio('sounds/crash.mp3');
+//      aud.play();
+//   }else if(this.innerHTML=='a'){
+//      var aud=new Audio('sounds/kick-bass.mp3');
+//    aud.play();
+// }
+// else if(this.innerHTML=='s'){
+//    var aud=new Audio('sounds/snare.mp3');
+//  aud.play();
+// }
+// else if(this.innerHTML=='d'){
+//    var aud=new Audio('sounds/tom-1.mp3');
+//  aud.play();
+// }
+// else if(this.innerHTML=='j'){
+//    var aud=new Audio('sounds/tom-2.mp3');
+//  aud.play();
+// }
+// else if(this.innerHTML=='k'){
+//    var aud=new Audio('sounds/tom-3.mp3');
+//  aud.play();
+// }
+// else if(this.innerHTML=='l'){
+//    var aud=new Audio('sounds/tom-4.mp3');
+//  aud.play();
+// }
   }
